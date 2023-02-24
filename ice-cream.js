8b0904bf6b663bbd37798b6089d94aa82ac41dda
Nanaya Miki <nanayamiki@gmail.com>
2023-02-24 03:10:45 +0000

    added ice-cream.js file. array push method

---
+++ ice-cream.js
@@ -0,0 +1,15 @@
+// Do not modify these first two lines
+let iceCreamFlavours = [
+	"chocolate",
+	"vanilla",
+	"cookies and cream",
+	"rocky road",
+	"strawberry",
+];
+console.log(iceCreamFlavours);
+iceCreamFlavours.push("root beer");
+
+console.log(iceCreamFlavours); // Your code below here...
+//console.log(result[0])
+//console.log(result[result.length - 1])
+//console.log(result.length)
