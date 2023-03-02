892a15cc399221daf9884e1745e822dc9030ff8f
Nanaya Miki <nanayamiki@gmail.com>
2023-03-02 14:50:44 +0000

    added percentEncoded.js

--- instructorWithLongestName.js
+++ instructorWithLongestName.js
@@ -1,15 +1,30 @@
 const instructorWithLongestName = function (instructors) {
-	// Put your solution here
+	//create a variable to start the longest length
+	//  loop the array of objects
+	// compare two elements each iteration and return true if it's longer
+	let longestName = 0;
+	console.log(instructors);
+
+	for (let i = 1; i < instructors.length; i++) {
+		//4 loops, 3loops
+		if (instructors[i].name.length > instructors[longestName].name.length) {
+			// console.log(instructors[i].name)
+			longestName = i;
+		}
+	}
+	return instructors[longestName];
 };
 
 console.log(
 	instructorWithLongestName([
 		{ name: "Samuel", course: "iOS" },
-		{ name: "Jeremiah", course: "Web" },
+		{ name: "Jeremddd", course: "Web" },
 		{ name: "Ophilia", course: "Web" },
 		{ name: "Donald", course: "Web" },
 	])
 );
+//{ name: 'Jeremddd', course: 'Web' }
+
 console.log(
 	instructorWithLongestName([
 		{ name: "Matthew", course: "Web" },
@@ -17,3 +32,5 @@ console.log(
 		{ name: "Domascus", course: "Web" },
 	])
 );
+
+//{ name: 'Domascus', course: 'Web' }
