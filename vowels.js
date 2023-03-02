d3ca3831cb76e2ca3673ccc9164accba192c9413
Nanaya Miki <nanayamiki@gmail.com>
2023-03-02 04:40:52 +0000

    added vowels.js with solutions

--- vowels.js
+++ vowels.js
@@ -17,3 +17,20 @@ const numberOfVowels = function (data) {
 console.log(numberOfVowels("orange"));
 console.log(numberOfVowels("lighthouse labs"));
 console.log(numberOfVowels("aeiou"));
+
+//or
+
+const numberOfVowels2 = function (data) {
+	const vowels = ["a", "e", "i", "o", "u"];
+	let count = 0;
+	for (let i = 0; i < data.length; i++) {
+		if (vowels.includes(data[i].toLowerCase())) {
+			count++;
+		}
+	}
+	return count;
+};
+
+console.log(numberOfVowels2("orange")); // output: 3
+console.log(numberOfVowels2("lighthouse labs")); // output: 5
+console.log(numberOfVowels2("aeiou")); // output: 5
