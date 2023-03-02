79d7364e02b4eeb82c561947e24808a6ef9eb7a3
Nanaya Miki <nanayamiki@gmail.com>
2023-03-02 03:55:34 +0000

    added vowels.js with my solution

--- vowels.js
+++ vowels.js
@@ -1,5 +1,17 @@
 const numberOfVowels = function (data) {
-	// Put your solution here
+	let arr = [];
+	for (i = 0; i < data.length; i++) {
+		if (
+			data[i] === "a" ||
+			data[i] === "i" ||
+			data[i] === "u" ||
+			data[i] === "e" ||
+			data[i] === "o"
+		) {
+			arr.push(data[i]);
+		}
+	}
+	return arr.length;
 };
 
 console.log(numberOfVowels("orange"));
