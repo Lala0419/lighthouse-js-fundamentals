8cd797be0aecc2b8ef60ba3c2d61f40289bafe2b
Nanaya Miki <nanayamiki@gmail.com>
2023-03-08 16:48:26 +0000

    added repeatingNumbers.js file

---
+++ repeatingNumbers.js
@@ -0,0 +1,17 @@
+// The input data for this exercise will be two dimensional array (an array of arrays), where each sub-array will have two numeric values. For example:
+
+// [[1, 2], [2, 3]]
+// The first will be the value to repeat, the second will be the amount of times to repeat that value.
+
+// Input
+// const repeatNumbers = function(data) {
+//   // Put your solution here
+// };
+
+// console.log(repeatNumbers([[1, 10]]));
+// console.log(repeatNumbers([[1, 2], [2, 3]]));
+// console.log(repeatNumbers([[10, 4], [34, 6], [92, 2]]));
+// Expected Output
+// 1111111111
+// 11, 222
+// 10101010, 343434343434, 9292
