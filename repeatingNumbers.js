328798e3058b84b66042317b2e36543e5ab61edc
Nanaya Miki <nanayamiki@gmail.com>
2023-03-08 16:49:07 +0000

    added repeatingNumbers.js file with my solution

--- repeatingNumbers.js
+++ repeatingNumbers.js
@@ -15,3 +15,38 @@
 // 1111111111
 // 11, 222
 // 10101010, 343434343434, 9292
+
+const repeatNumbers = function (data) {
+	let result = "";
+	for (let i = 0; i < data.length; i++) {
+		let value = data[i][0];
+		let mul = data[i][1];
+		for (let j = 0; j < mul; j++) {
+			result += value;
+		}
+		if (i < data.length - 1) {
+			//tricky here. was able to know nested loop was needed
+			result += ", ";
+		}
+	}
+	return result;
+};
+
+console.log(repeatNumbers([[1, 10]]));
+console.log(
+	repeatNumbers([
+		[1, 2],
+		[2, 3],
+	])
+);
+console.log(
+	repeatNumbers([
+		[10, 4],
+		[34, 6],
+		[92, 2],
+	])
+);
+
+// 1111111111
+// 11, 222
+// 10101010, 343434343434, 9292
