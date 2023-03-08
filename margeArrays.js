b18303ad35994a2df40953d869e51c5ad41bd22f
Nanaya Miki <nanayamiki@gmail.com>
2023-03-08 05:45:47 +0000

    added margeArray.js file

---
+++ margeArrays.js
@@ -0,0 +1,10 @@
+// Define a function called merge
+
+// The function should, when given two sorted arrays containing numbers, returns a sorted array of the numbers from both lists.
+
+// For example:
+
+// code	output
+// merge([ 4, 5, 6 ], [ 1, 2, 3, 4 ]);	[ 1, 2, 3, 4, 4, 5, 6 ]
+// merge([ 4 ], [ 2, 5, 8 ]);	[ 2, 4, 5, 8 ]
+// merge([ 1, 2, 6 ], []);	[ 1, 2, 6 ]
