753629d56ceaf1008e4ccc7adfe2aba67fb69740
Nanaya Miki <nanayamiki@gmail.com>
2023-03-09 15:29:53 +0000

    added caseMaker.js file

---
+++ caseMaker.js
@@ -0,0 +1,16 @@
+// Case Maker
+// We will receive a normal string of words separated with spaces as the input. Our job is to convert these strings into camel cased strings.
+
+// Input
+// const camelCase = function(input) {
+//   // Your code here
+// };
+
+// console.log(camelCase("this is a string"));
+// console.log(camelCase("loopy lighthouse"));
+// console.log(camelCase("supercalifragalisticexpialidocious"));
+// Output
+// thisIsAString
+// loopyLighthouse
+// supercalifragalisticexpialidocious
+// Instruction
