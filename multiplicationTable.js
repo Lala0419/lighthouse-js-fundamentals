9f119e8a4adcf94be3039b44db28cbc971ab284f
Nanaya Miki <nanayamiki@gmail.com>
2023-03-11 17:40:49 +0000

    added multiplicationTable.js file with gpt solution

--- multiplicationTable.js
+++ multiplicationTable.js
@@ -3,6 +3,17 @@
 // Multiplication Table
 // We will be given a number as our input data. This number is the highest value of our multiplication table. Our job is to generate a multiplication table for the values from 1 to the provided number.
 
+const multiplicationTable = function (maxValue) {
+	let table = "";
+	for (let i = 1; i <= maxValue; i++) {
+		for (let j = 1; j <= maxValue; j++) {
+			table += i * j + " ";
+		}
+		table += "\n";
+	}
+	return table;
+};
+
 // Output
 // 1
 
